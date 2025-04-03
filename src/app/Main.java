6d48f1ec41faf8e9db08fce724d11c760dcf49db
VladislavSCH <114906748+VladislavSCH@users.noreply.github.com>
2025-04-03 11:01:50 +0300

    Creating app Manipulating User Objects

---
+++ src/app/Main.java
@@ -0,0 +1,19 @@
+package app;
+
+import java.util.List;
+import java.util.Optional;
+
+public class Main {
+    public static void main(String[] args) {
+        UserRepository userRepository = new UserRepository();
+
+        Optional<User> userId = userRepository.findUserById(3);
+        Optional<User> userEmail = userRepository.findUserByEmail("john@gmail.com");
+        Optional<List<User>> userList = userRepository.findAllUser();
+
+        System.out.println(userId);
+        System.out.println(userEmail);
+        System.out.println("___________________________________");
+        System.out.println(userList);
+    }
+}
---
+++ src/app/UserRepository.java
@@ -0,0 +1,39 @@
+package app;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class UserRepository {
+
+    public List<User> getDataUser(){
+        return Arrays.asList(
+                new User(1, "Vlad", "vlad@gmail.com"),
+                new User(2, "Stas", "stas@gmail.com"),
+                new User(3, "Sveta", "sveta@gmail.com"),
+                new User(4, "Nikola", "nikola@gmail.com"),
+                new User(5, "John", "john@gmail.com")
+        );
+    }
+
+   public Optional<User> findUserById(int id){
+        return getDataUser().stream()
+                .filter(User -> User.getId() == id)
+                .findFirst();
+   }
+
+   public Optional<User> findUserByEmail(String email){
+        return getDataUser().stream()
+                .filter(User -> User.getEmail() == email)
+                .findFirst();
+   }
+
+   public Optional<List<User>> findAllUser(){
+        List<User> list = getDataUser();
+        if(list.isEmpty()){
+            return Optional.empty();
+        }else {
+            return Optional.of(list);
+        }
+   }
+}
